1d208196ad2d5aa6de2762f09b0b0ce563fb9111
Leo Rigotto <leorigotto@me.com>
2022-03-08 16:37:41 -0300

    PokeCard criado

--- pokedex/src/components/PokeCard/PokeCard.js
+++ pokedex/src/components/PokeCard/PokeCard.js
@@ -1,3 +1,36 @@
+import { Button } from "@mui/material";
+import { getThemeProps } from "@mui/system";
+import { useState } from "react";
+import { CardContainer, CardFooter } from "./styledPokeCard";
 
 
+export default function PokeCard(props) {
+    const isOnPokedex = () => {
+        // checks if pokemon is already on pokedex and returns a boolean
+        return false
+    }
 
+    return <CardContainer bgColor={props.bgColor ? props.bgColor : 'white'}>
+        <p>Nome: {props.name}</p>
+        <img src="http://pm1.narvii.com/6434/7a2cb5fc86df1db37db549422128c66186059808_00.jpg" alt="pokemon" />
+        <p>Tipo: {props.type}</p>
+        <CardFooter>
+            <Button 
+                color="secondary" 
+                variant="contained" 
+                disabled={isOnPokedex()}
+                size="medium"
+            >
+                add
+            </Button>
+            <Button 
+                variant="outlined"
+                color="secondary" 
+                size="medium"
+                href={`/detalhes/${props.id}`}
+            >
+                detalhes
+            </Button>
+        </CardFooter>
+    </CardContainer>
+}
---
+++ pokedex/src/components/PokeCard/styledPokeCard.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export const CardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
+    width: 200px;
+    height: 250px;
+    background-color: ${props => props.bgColor} ;
+    border: 1px solid lightgray;
+    img{
+        height: 170px;
+    }
+`
+
+export const CardFooter = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    width: 100%;
+    background-color: 'white';
+`
