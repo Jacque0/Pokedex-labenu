2d5dfc438cee437575adb3f323a3335b0a732242
Jacqueline Nascimento <jacqueline.cmessias@gmail.com>
2022-03-11 12:00:05 -0300

    header fixo

--- pokedex/src/components/Header/styleHeader.js
+++ pokedex/src/components/Header/styleHeader.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 export const ContainerHeader = styled.div`
+    position: sticky;
+    top: 0;
+    z-index: 2;
     .extra-button{
         display: ${props => props.display};
     }
